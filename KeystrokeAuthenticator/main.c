e3e3977da289691dd7cda95e4c0bf5f925cf534d
Logician724 <king2sairos2013@gmail.com>
2018-11-05 14:24:52 +0200

    chore: Init atmel atmega16A project

---
+++ KeystrokeAuthenticator/main.c
@@ -0,0 +1,18 @@
+/*
+ * KeystrokeAuthenticator.c
+ *
+ * Created: 11/5/2018 11:10:19 AM
+ * Author : Army-O
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
